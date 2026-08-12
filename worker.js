234d273563da33676f0ef1c220b64d8313bdba31
adisingh0824 <saditya7990@gmail.com>
2026-08-12 22:53:41 +0530

    feat: serve static assets from worker.js using Cloudflare Worker Assets binding

---
+++ worker.js
@@ -0,0 +1,7 @@
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    // Serve static assets using the built-in ASSETS binding
+    return env.ASSETS.fetch(request);
+  }
+};
